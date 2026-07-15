b5821b4b28e0bca82f9668987aae24c06733a40e
Issa Abubakar <abubakarissa47722@gmail.com>
2026-07-15 13:27:42 +0100

    Initial Commit

---
+++ Sotto/SottoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SottoApp.swift
+//  Sotto
+//
+//  Created by Issa Abubakar on 15/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct SottoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
